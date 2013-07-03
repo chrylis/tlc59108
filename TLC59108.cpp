16d8b6902df5636b93971fb00eb88672b590063c
Christopher Smith <chrylis@gmail.com>
2013-07-02 23:59:38 -0500

    add LGPL license header

--- TLC59108.cpp
+++ TLC59108.cpp
@@ -1,3 +1,22 @@
+/*
+ * TLC59108: Arduino library to control TI TLC59108/TLC59108F/TLC59208 LED drivers
+ * 
+ * (C) 2013 Christopher Smith <chrylis@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 extern "C"
 {
 #include <inttypes.h>
